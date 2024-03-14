a2206c7a17088a066bcfe4f6b551acd7f93e56b8
Kənan Muradov <maljerov.1999@gmail.com>
2024-03-14 15:58:12 +0300

    first commit

---
+++ src/app/component/page.jsx
@@ -0,0 +1,10 @@
+import DemoComponent from '@/components/DemoComponent'
+import React from 'react'
+
+const Component = () => {
+    return (
+        <DemoComponent />
+    )
+}
+
+export default Component
---
+++ src/components/DemoComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DemoComponent() {
+  return (
+    <div>My Component</div>
+  )
+}
+
+export default DemoComponent
